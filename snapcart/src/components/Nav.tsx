ad32e52c71a27223fa737073b6cabcece2836e0b
vikashrao5464 <vikashrao5464@gmail.com>
2026-01-25 12:09:53 +0530

    add order summary section

--- snapcart/src/components/Nav.tsx
+++ snapcart/src/components/Nav.tsx
@@ -83,7 +83,7 @@ function Nav({ user }: { user: IUser }) {
     </AnimatePresence>, document.body
   ) : null
   return (
-    <div className='w-[95%] fixed top-4 left-1/2 -translate-x-1/2 bg-gradient-to-r from-green-500  to-green-700 rounded-2xl shadow-lg shadow-black/30 flex justify-between items-center h-20 px-4 md:px-8 z-50 ' >
+    <div className='w-[95%] fixed top-4 left-1/2 -translate-x-1/2 bg-linear-to-r from-green-500  to-green-700 rounded-2xl shadow-lg shadow-black/30 flex justify-between items-center h-20 px-4 md:px-8 z-50 ' >
 
       <Link href={"/"} className='text-white font-extrabold text-2xl sm:text-3xl tracking-wide hover:scale-105 transition-transform '>snapCart
       </Link>
@@ -103,7 +103,7 @@ function Nav({ user }: { user: IUser }) {
 
         </div>
 
-          <Link href={""} className='relative bg-white rounded-full w-11 h-11 flex items-center justify-center shadow-md hover:scale-105 transition'>
+          <Link href={"/user/cart"} className='relative bg-white rounded-full w-11 h-11 flex items-center justify-center shadow-md hover:scale-105 transition'>
             <ShoppingCartIcon className='text-green-600 w-6 h-6 ' />
             <span className='absolute -top-1 -right-1 bg-red-500 text-white text-xs w-5 h-5 flex items-center justify-center rounded-full font-semibold shadow '>{cartData.length}</span>
           </Link></>}
--- snapcart/src/redux/cartSlice.ts
+++ snapcart/src/redux/cartSlice.ts
@@ -18,10 +18,16 @@ interface IGrocery{
 }
 
 interface IcartSlice{
-  cartData:IGrocery[] 
+  cartData:IGrocery[],
+  subTotal:number,
+  deliveryFee:number,
+  finalTotal:number
 }
 const initialState:IcartSlice={
-cartData:[]
+  cartData:[],
+  subTotal: 0,
+  deliveryFee: 40,
+  finalTotal: 40
 }
 
 const cartSlice=createSlice({
@@ -32,14 +38,21 @@ const cartSlice=createSlice({
     // here state is the current state of the user slice and action is the data which we want to store in the user slice and payload is the actual data inside the action
     addToCart:(state,action:PayloadAction<IGrocery>)=>{
       state.cartData.push(action.payload)
+      // call calculateTotals reducer to update the total amount whenever an item is added to the cart
+      cartSlice.caseReducers.calculateTotals(state)
     },
+
     // action payload is the id of the item to be added to the cart
     increaseQuantity:(state,action:PayloadAction<mongoose.Types.ObjectId>)=>{
       const item=state.cartData.find(i=>i._id===action.payload)
       if(item){
         item.quantity=item.quantity+1;
       }
+
+      // call calculateTotals reducer to update the total amount whenever an item quantity is increased
+      cartSlice.caseReducers.calculateTotals(state)
     },
+
     decreaseQuantity:(state,action:PayloadAction<mongoose.Types.ObjectId>)=>{
       const item=state.cartData.find(i=>i._id===action.payload)
       if(item?.quantity && item.quantity>1){
@@ -48,11 +61,27 @@ const cartSlice=createSlice({
         // remove the item from the cart if quantity is 1
        state.cartData = state.cartData.filter(i=>i._id!==action.payload)
       }
+      // call calculateTotals reducer to update the total amount whenever an item quantity is decreased
+      cartSlice.caseReducers.calculateTotals(state)
+    },
+
+    removeFromCart:(state,action:PayloadAction<mongoose.Types.ObjectId>)=>{
+      state.cartData = state.cartData.filter(i=>i._id!==action.payload)
+      // call calculateTotals reducer to update the total amount whenever an item is removed from the cart
+      cartSlice.caseReducers.calculateTotals(state)
+    },
+
+    // reducres for calculating the total amount of the cart
+    calculateTotals:(state)=>{
+      // here reduce is used to iterate through the cartData array and calculate the total amount,its initial value is 0 which is passed as second argument to reduce
+      state.subTotal=state.cartData.reduce((sum,item)=>sum+Number(item.price)*item.quantity,0)
+      state.deliveryFee=state.subTotal>100?0:40
+      state.finalTotal=state.subTotal+state.deliveryFee
+      // this reducers will be called whenever there is any change in the cartData array like adding or removing item or increasing or decreasing quantity ,so we will call this reducer in every other reducer
     }
   }
-}
-)
+})
 // exporting the action to be used in the components
-export const {addToCart,increaseQuantity,decreaseQuantity}=cartSlice.actions
+export const {addToCart,increaseQuantity,decreaseQuantity,removeFromCart}=cartSlice.actions
 // exporting the reducer to be used in the store
 export default cartSlice.reducer
