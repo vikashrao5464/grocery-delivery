3d1daa932bc6539a44ff9abafb5c2aad293b8d87
vikashrao5464 <vikashrao5464@gmail.com>
2026-02-11 15:57:49 +0530

    Change directory

--- snapcart/src/global.d.ts
+++
@@ -1,9 +0,0 @@
-import { Connection } from "mongoose";
-
-declare global{
-  var mongoose:{
-    conn:Connecton | null;
-    promise:Promise<Connection> | null;
-  }
-}
-export {};
--- snapcart/src/next-auth.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "next-auth" {
-  interface User{
-    id:string,
-    name:string,
-    email:string,
-    role:string
-  }
-}
-export {};
